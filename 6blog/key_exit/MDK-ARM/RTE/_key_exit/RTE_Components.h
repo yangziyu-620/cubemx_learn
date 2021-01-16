51288873c7376c00e0be2809b1db500229a72426
杨子裕 <57806700+yangziyu-620@users.noreply.github.com>
2021-01-16 18:12:21 +0800

    new blog update

---
+++ 6blog/key_exit/MDK-ARM/RTE/_key_exit/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'key_exit' 
+ * Target:  'key_exit' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
