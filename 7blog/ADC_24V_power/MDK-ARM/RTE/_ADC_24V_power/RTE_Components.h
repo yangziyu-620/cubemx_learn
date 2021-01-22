5fb80b74c81c3aa9757ec28083fec101e17e7f5a
杨子裕 <57806700+yangziyu-620@users.noreply.github.com>
2021-01-22 09:08:33 +0800

    new blog

---
+++ 7blog/ADC_24V_power/MDK-ARM/RTE/_ADC_24V_power/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'ADC_24V_power' 
+ * Target:  'ADC_24V_power' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
